775889e9db504357327902f698b3877b766386c0
Boujieali <adugyamfialice87@gmail.com>
2023-08-10 14:55:02 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *  main - this is the main function of the program
+ *
+ *  Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammer, nut the outcome is a piece of art,\n");
+	return (0);
+}
